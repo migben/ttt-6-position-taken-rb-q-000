38e5a7bce34c3f2925c23126489db298197dec1d
Miguel Ben <codexheartdevs@gmail.com>
2019-11-25 03:13:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, idx)
+  if board[idx] == " " || board[idx] == ""
+    return false
+  else
+    return true
+  end
+end
+    
